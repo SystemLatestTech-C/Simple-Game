ef3a849215cabaa3b4e71274b5311a681a20487a
20191570 <rlawogns0513@kookmin.ac.kr>
2023-04-23 11:40:49 +0900

    :sparkles: server, player1, player2 통합
    한 코드에서 서버와 플레이어의 기능을 전부 수행하도록 코드를 통합했습니다. 또한 상수, 함수(move_racket, randomize_vec), server, MainState, PlayerState로 기능별로 모듈을 분리했습니다.

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,68 @@
-use std::error::Error;
-use std::net::TcpListener;
+use ggez; // rust의 게임 라이브러리
+use ggez::event; // 이벤트 모듈
+use ggez::graphics; // 그래픽 모듈
+use ggez::input::keyboard::{self, KeyCode}; // 키보드 모듈
+use ggez::{Context, GameResult}; // 게임 모듈(실행환경 저장 및 결과 반환)
 use std::thread;
 use std::io::{Read, Write};
+use std::env;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
-    println!("Server listening on 127.0.0.1:8080");
+//use ggez::nalgebra as na; // 벡터, 행렬 등의 수학 연산 모듈
+//use std::net::TcpStream;
+//use std::io::prelude::*;
+//use std::error::Error;
+//use std::net::TcpListener;
+//use std::io;
+//use rand::{self, thread_rng, Rng}; // 랜덤 모듈
+//use std::io::{ErrorKind};
 
-    let (mut client_1_socket, _) = listener.accept()?;
-    println!("Client 1 connected");
+mod constants; // 상수를 관리하는 모듈입니다.
+mod state_func;  // move_racket, randomize_vec 함수를 관리하는 모듈입니다.
+mod server;     // 서버를 관리하는 모듈입니다.
+mod main_state;  // 플레이어1을 관리하는 모듈입니다.
+mod player_state;  // 플레이어2를 관리하는 모듈입니다.
 
-    let (mut client_2_socket, _) = listener.accept()?;
-    println!("Client 2 connected");
+use constants::*;
+use state_func::*;
+use server::listen_for_clients;
+use main_state::MainState;
+use player_state::PlayerState;
 
-    let mut client_1_socket_clone = client_1_socket.try_clone()?;
-    let mut client_2_socket_clone = client_2_socket.try_clone()?;
+/**
+*   main 함수입니다.
+*   처음 4줄은 이전에 작성한 main과 동일합니다.
+*   그 후, 실행 인자에 따라서 서버+호스트(플레이어1)를 실행할지, 플레이어2를 실행할지를 결정합니다.
+*   cargo run -- host 로 실행할 시 서버를 실행하고 MainState를 사용해서 실행합니다.
+*   cargo run -- player 로 실행할 시 플레이어2를 실행하고 PlayerState를 사용해서 실행합니다.
+*
+*   주의점 : 서버는 호스트가 실행될 때 시작하기 때문에, cargo run -- host 를 먼저 실행해야 합니다.
+*/
 
-    let client_1_to_client_2 = thread::spawn(move || {
-        let mut buffer = [0; 4];
-        loop {
-            match client_1_socket.read_exact(&mut buffer) {
-                Ok(_) => {
-                    if let Err(e) = client_2_socket.write_all(&buffer) {
-                        println!("Error sending data to client 2: {}", e);
-                        break;
-                    }
-                }
-                Err(e) => {
-                    println!("Error reading data from client 1: {}", e);
-                    break;
-                }
-            }
+fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("Ping-pong", "Name")
+        .window_mode(ggez::conf::WindowMode::default().dimensions(800.0, 600.0));
+    let (ctx, event_loop) = &mut cb.build()?;
+    graphics::set_window_title(ctx, "Ping-Pong"); // 창 제목을 설정합니다.
+
+    let args: Vec<String> = env::args().collect();
+    match args[1].as_str() {
+        "host" => {
+            println!("호스트 접속");
+            let server_thread = thread::spawn(|| {
+                listen_for_clients();
+            });
+            let mut state = MainState::new(ctx); // Mainstate를 초기화합니다.
+            event::run(ctx, event_loop, &mut state).unwrap();
         }
-    });
-
-    let client_2_to_client_1 = thread::spawn(move || {
-        let mut buffer = [0; 4];
-        loop {
-            match client_2_socket_clone.read_exact(&mut buffer) {
-                Ok(_) => {
-                    if let Err(e) = client_1_socket_clone.write_all(&buffer) {
-                        println!("Error sending data to client 1: {}", e);
-                        break;
-                    }
-                }
-                Err(e) => {
-                    println!("Error reading data from client 2: {}", e);
-                    break;
-                }
-            }
+        "player" => {
+            println!("플레이어2 접속");
+            let mut state = PlayerState::new(ctx); // PlayerState를 초기화합니다.
+            event::run(ctx, event_loop, &mut state).unwrap();
         }
-    });
+        _ => {
+            println!("Usage: cargo run -- [server|player1|player2]");
+        }
+    }
 
-    client_1_to_client_2.join().unwrap();
-    client_2_to_client_1.join().unwrap();
-
-    Ok(())
+    Ok(()) //성공적으로 완료, 반환값 없음
 }
