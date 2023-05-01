8170eba9c5a619a4e2d356901b46d683d2a4c13e
dup06570 <dup06570@kookmin.ac.kr>
2023-05-01 18:10:54 +0900

    :art: 버튼 기능 변경
    stay_state에 버튼 text를 player1과 player2로 변경했습니다. 포트 입력 기능을 제거하기로 해서 input_state를 통해서 가지 않고 바로 client로 가게 했습니다.

--- src/stay_state.rs
+++ src/stay_state.rs
@@ -11,10 +11,10 @@ pub struct StayState {
     pub state_transition: StateTransition, //스테이트를 변경하고자 할 때 변경될 ENUM 값
     multi_title_text: graphics::Text,            //멀티타이틀 텍스트
     multi_title_text_pos: na::Point2<f32>,       //멀티타이틀 텍스트 위치
-    make_room_button_text: graphics::Text,    //host 시작 버튼 텍스트
-    make_room_button_pos: na::Point2<f32>,    //host 시작 버튼 위치
-    input_port_button_text: graphics::Text,     //port 번호 입력 state 시작 버튼 텍스트
-    input_port_button_post: na::Point2<f32>,     //port 번호 입력 state 시작 버튼 위치
+    player1_start_button_text: graphics::Text,    //host 시작 버튼 텍스트
+    player1_start_button_pos: na::Point2<f32>,    //host 시작 버튼 위치
+    player2_start_button_text: graphics::Text,     //port 번호 입력 state 시작 버튼 텍스트
+    player2_start_button_post: na::Point2<f32>,     //port 번호 입력 state 시작 버튼 위치
 }
 
 impl StayState {
@@ -32,21 +32,21 @@ impl StayState {
         //  host 시작 버튼 텍스트 설정
         let button_font = graphics::Font::default();
         let button_scale = graphics::Scale::uniform(20.0);
-        let make_room_button_text = graphics::Text::new(
-            graphics::TextFragment::new("Start host")
+        let player1_start_button_text = graphics::Text::new(
+            graphics::TextFragment::new("Player1")
                 .font(button_font)
                 .scale(button_scale)
                 .color(graphics::WHITE),
         );
-        let make_room_button_text_width = make_room_button_text.width(ctx) as f32;
+        let player1_start_button_text_width = player1_start_button_text.width(ctx) as f32;
         //port번호 입력 state 시작 버튼 텍스트 설정
-        let input_port_button_text = graphics::Text::new(
-            graphics::TextFragment::new("Input port number")
+        let player2_start_button_text = graphics::Text::new(
+            graphics::TextFragment::new("Player2")
                 .font(button_font)
                 .scale(button_scale)
                 .color(graphics::WHITE),
         );
-        let input_port_button_text_width = input_port_button_text.width(ctx) as f32;
+        let player2_start_button_text_width = player2_start_button_text.width(ctx) as f32;
 
         let (screen_w, screen_h) = graphics::drawable_size(ctx); // 현재 게임 윈도우의 너비와 높이 정보를 screen_w, screen_h에 저장
 
@@ -57,14 +57,14 @@ impl StayState {
                 screen_w * 0.5 - multi_title_text_width * 0.5,
                 screen_h * 0.1,
             ),
-            make_room_button_text,                      //버튼 텍스트
-            make_room_button_pos: na::Point2::new(
-                screen_w * 0.5 - (input_port_button_text_width* 0.5 +(make_room_button_text_width)),
+            player1_start_button_text,                      //버튼 텍스트
+            player1_start_button_pos: na::Point2::new(
+                screen_w * 0.5 - (player2_start_button_text_width* 0.5 +(player1_start_button_text_width)),
                 screen_h * 0.5 ,
             ), //버튼 위치
-            input_port_button_text,
-            input_port_button_post: na::Point2::new(
-                screen_w * 0.5 + input_port_button_text_width * 0.5,
+            player2_start_button_text,
+            player2_start_button_post: na::Point2::new(
+                screen_w * 0.5 + player2_start_button_text_width * 0.5,
                 screen_h * 0.5,
             ),
         }
@@ -81,26 +81,26 @@ impl event::EventHandler for StayState {
     ) {
         let point = na::Point2::new(x, y); //마우스의 좌표 가져오기
 
-        let make_room_button_rect = graphics::Rect::new(
-            self.make_room_button_pos.x,
-            self.make_room_button_pos.y,
-            self.make_room_button_text.width(ctx) as f32,
-            self.make_room_button_text.height(ctx) as f32,
+        let player1_start_button_rect = graphics::Rect::new(
+            self.player1_start_button_pos.x,
+            self.player1_start_button_pos.y,
+            self.player1_start_button_text.width(ctx) as f32,
+            self.player1_start_button_text.height(ctx) as f32,
         ); //버튼으로 설정할 사각형 영역 생성
 
-        let multi_button_rect = graphics::Rect::new(
-            self.input_port_button_post.x,
-            self.input_port_button_post.y,
-            self.input_port_button_text.width(ctx) as f32,
-            self.input_port_button_text.height(ctx) as f32,
+        let player2_start_button_rect = graphics::Rect::new(
+            self.player2_start_button_post.x,
+            self.player2_start_button_post.y,
+            self.player2_start_button_text.width(ctx) as f32,
+            self.player2_start_button_text.height(ctx) as f32,
         );
 
         //설정한 사각형 좌표에 마우스 클릭 좌표가 겹치면 게임 페이지 전환.
-        if make_room_button_rect.contains(point) {
+        if player1_start_button_rect.contains(point) {
             self.state_transition = StateTransition::Host; //client가 들어올 때 까지 기다리는 state로 전환
         }
-        if multi_button_rect.contains(point) {
-            self.state_transition = StateTransition::Input; //port 번호를 입력 받는 state로 전환
+        if player2_start_button_rect.contains(point) {
+            self.state_transition = StateTransition::Client; //port 번호를 입력 받는 state로 전환
         }
     }
 
@@ -115,11 +115,11 @@ impl event::EventHandler for StayState {
         draw_param.dest = self.multi_title_text_pos.into();
         graphics::draw(ctx, &self.multi_title_text, draw_param)?;
 
-        draw_param.dest = self.make_room_button_pos.into();
-        graphics::draw(ctx, &self.make_room_button_text, draw_param)?;
+        draw_param.dest = self.player1_start_button_pos.into();
+        graphics::draw(ctx, &self.player1_start_button_text, draw_param)?;
 
-        draw_param.dest = self.input_port_button_post.into();
-        graphics::draw(ctx, &self.input_port_button_text, draw_param)?;
+        draw_param.dest = self.player2_start_button_post.into();
+        graphics::draw(ctx, &self.player2_start_button_text, draw_param)?;
 
         graphics::present(ctx); // 현재 프레임을 출력, 이 부분이 없으면 최종적으로 화면에 그려지지 않음
         Ok(())
